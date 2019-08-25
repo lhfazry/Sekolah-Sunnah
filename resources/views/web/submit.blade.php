24171a998ae2452843d65339d2fe48ba1b944d9e
Fazri <lhuqita.fazry@gmail.com>
2019-08-25 10:58:03 +0700

    fixing city

--- resources/views/web/submit.blade.php
+++ resources/views/web/submit.blade.php
@@ -154,7 +154,7 @@
                             <div class="col-md-3 col-sm-3">
                                 <div class="form-group">
                                     {!! Form::label('city_id', 'Kabupaten/Kota', ['class' => 'col-form-label']) !!}
-                                    {!! Form::select('city_id', [], null, ['required']) !!}
+                                    {!! Form::select('city_id', [], null, []) !!}
                                 </div>
                             </div>
 
