bab191f75d662fa8b2384524f4b4f5248cda3eb0
klanjabrik <meirza.arson@sebangsa.com>
2019-10-01 13:47:33 +0700

    Add title di Uang  SPP dan Pendaftaran

--- resources/views/web/schoolcard.blade.php
+++ resources/views/web/schoolcard.blade.php
@@ -7,10 +7,10 @@
             <a href="{{route('web.detail', $school->slug_sekolah)}}" class="image-wrapper background-image"><img src="{{$school->getPhotoCoverUrl()}}" alt=""></a>
         </div>
         <h4 class="location"><a href="{{route('web.search', ['city'=>$school->city_id])}}"><small>{{$school->city_province()}}</small></a></h4>
-        <div class="price">{{$school->displaySPP()}}</div>
+        <div class="price" title="SPP">{{$school->displaySPP()}}</div>
         <div class="meta">
             {!!$school->getOtherFacilities()!!}
-            <figure><i class="fa fa-money"></i>&nbsp;{{$school->displaySPP()}}</figure>
+            <figure title="Uang Pendaftaran"><i class="fa fa-money"></i>&nbsp;{{$school->displayBiayaPendaftaran()}}</figure>
         </div>
         <div class="description">
             <p>{{$school->exceprt()}}</p>
