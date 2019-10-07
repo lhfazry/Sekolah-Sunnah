0817907dfa63cc3565096cd8c1a3ce44c7370d13
klanjabrik <meirza.arson@sebangsa.com>
2019-10-07 15:09:24 +0700

    Fix search results with facilities

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -85,7 +85,7 @@ class WebController extends AppBaseController
         $facilities = \App\Models\Facility::where('display', true)->get();
         $cities = $this->getCities();
 
-        $schools = \App\Models\School::orderBy('created_at', 'desc')
+        $schools = \App\Models\School::orderBy('schools.created_at', 'desc')
             ->where('status', 'Published');
 
         if(!empty($keyword)) {
@@ -97,12 +97,12 @@ class WebController extends AppBaseController
         }
 
         if(!isset($facilities_form)) $facilities_form=array();
-        
         if(count($facilities_form) > 0) {
-            $schools->whereHas('facilities', function($query) use($facilities_form) {
-                $query->whereIn('facility_id', $facilities_form);
-            });
-            if(count($facilities_form) > 1) $schools->havingRaw('count(distinct id) = ?', [count($facilities_form)]);
+            foreach ($facilities_form as $ff) {
+                $schools->whereHas('facilities', function ($query) use ($ff) {     
+                    $query->where('facility_id', $ff);
+                });
+            }
             $expand = true;
         }
 
